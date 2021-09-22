da4971ed76ee11c37242f7d55e5f867fd4a00c2c
Allan-Pahn <pahnallan@gmail.com>
2021-09-22 09:37:15 -0700

    Add solution to 2D array hourglass problem

---
+++ InterviewPrepKit/Arrays/2D Arrays/main.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    cache = []
+
+    maxSum = None
+
+    for y in range(4):
+        for x in range(4):
+            hourglassSum = calculateHourGlassSum(arr, x, y)
+            cache.append(hourglassSum)
+            if (maxSum == None or hourglassSum > maxSum):
+                maxSum = hourglassSum
+
+    return maxSum
+
+
+def calculateHourGlassSum(arr, y, x):
+    hourGlass0 = arr[y][x] + arr[y][x + 1] + arr[y][x + 2]
+    hourGlass1 = arr[y + 1][x + 1]
+    hourGlass2 = arr[y + 2][x] + arr[y + 2][x + 1] + arr[y + 2][x + 2]
+    return hourGlass0 + hourGlass1 + hourGlass2
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
